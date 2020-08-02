3838f73bace4f7b8f5457c9a514e17888e2a14e9
Jorge Viera <joviera@gmail.com>
2020-08-02 09:19:44 -0500

    Refactors save_image to be inside ClipboardWatcher class

--- ClipboardWatcher.py
+++ ClipboardWatcher.py
@@ -1,12 +1,16 @@
 import time
 import threading
+from datetime import datetime
+from os import path
+from uuid import uuid4
 from PIL import ImageGrab
 
+IMAGE_FOLDER = 'img'
+
 class ClipboardWatcher(threading.Thread):
-    def __init__(self, callback, pause=1.):
+    def __init__(self, pause=1.):
         super(ClipboardWatcher, self).__init__()
 
-        self._callback = callback
         self._pause = pause
         self._stopping = False
 
@@ -16,8 +20,19 @@ class ClipboardWatcher(threading.Thread):
             tmp_value = ImageGrab.grabclipboard()
             if tmp_value != None and tmp_value != recent_value:
                 recent_value = tmp_value
-                self._callback(recent_value)
+                self.save_image(recent_value)
             time.sleep(self._pause)
 
     def stop(self):
+        now = datetime.now()
+        print (f'{now.strftime("%Y/%m/%d %H:%M:%S")} - Stopping, caused by Keyboard Interrupt.')
         self._stopping = True
+
+    def save_image(self, clipboard_content):
+        now = datetime.now()
+        try:
+            filename = str(uuid4()).replace('-','')+'.png'
+            clipboard_content.save(path.join(IMAGE_FOLDER, filename), 'PNG')
+            print (f'{now.strftime("%Y/%m/%d %H:%M:%S")} - Found a new image, saving it locally.')
+        except:
+            print (f'{now.strftime("%Y/%m/%d %H:%M:%S")} - Found a new image, but couldn\'t save it.')
--- clipboard_monitor.py
+++ clipboard_monitor.py
@@ -1,23 +1,8 @@
 import time
-from datetime import datetime
-from os import path
-from uuid import uuid4
 from ClipboardWatcher import ClipboardWatcher
 
-IMAGE_FOLDER = 'img'
-
-def save_image(clipboard_content):
-    now = datetime.now()
-    try:
-        filename = str(uuid4()).replace('-','')+'.png'
-        clipboard_content.save(path.join(IMAGE_FOLDER, filename), 'PNG')
-        print (f'{now.strftime("%Y/%m/%d %H:%M:%S")} - Found a new image, saving it locally.')
-    except:
-        print (f'{now.strftime("%Y/%m/%d %H:%M:%S")} - Found a new image, but couldn\'t save it.')
-
 def main():
-    watcher = ClipboardWatcher( save_image,
-                                .5)
+    watcher = ClipboardWatcher(.5)
     watcher.start()
     while True:
         try:
@@ -26,7 +11,6 @@ def main():
         except KeyboardInterrupt:
             watcher.stop()
             break
-
 
 if __name__ == "__main__":
     main()
